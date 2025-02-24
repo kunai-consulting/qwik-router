eee974129b4069eb1312c584344b848281ddf5e2
thejackshelton <me@jackshelton.com>
2025-02-24 00:39:45 -0600

    default export

--- src/server/server.tsx
+++ src/server/server.tsx
@@ -2,7 +2,7 @@ import { renderToString } from "@qwik.dev/core/server";
 import { manifest } from "@qwik-client-manifest";
 import App from "./routes/index";
 
-export async function render() {
+export default async function render() {
   return await renderToString(<App />, {
     manifest,
   });
